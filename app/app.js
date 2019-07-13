b19045a294f8bf1f9a14649c1485eeea60567e58
rndev80 <rndev80@gmail.com>
2019-07-13 09:53:04 +0300

    Bug fix on removing webflow

--- app/app.js
+++ app/app.js
@@ -28,9 +28,6 @@ import LanguageProvider from 'containers/LanguageProvider';
 import '!file-loader?name=[name].[ext]!./images/favicon.ico';
 import 'file-loader?name=.htaccess!./.htaccess'; // eslint-disable-line import/extensions
 
-import '!file-loader?name=[name].[ext]!./scripts/jquery-3.3.1.min.js';
-import '!file-loader?name=[name].[ext]!./scripts/webflow.js';
-
 import configureStore from './configureStore';
 
 // Import i18n messages
